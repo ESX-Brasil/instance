298da6dd29801bfbe132bc199b9b9565a8a61eb0
renildomarcio <krsolucoesweb@gmail.com>
2020-03-31 19:37:36 -0300

    Update br.lua

--- locales/br.lua
+++ locales/br.lua
@@ -1,8 +1,8 @@
 Locales['br'] = {
   ['left_instance'] = 'você deixou a instância',
   ['invite_expired'] = 'convite expirado',
-  ['press_to_enter'] = 'pressione ~INPUT_CONTEXT~ para entrar na instância',
+  ['press_to_enter'] = 'Pressione ~INPUT_CONTEXT~ para entrar na instância',
   ['entered_instance'] = 'você entrou na instância',
-  ['entered_into'] = '%s entered the instance',
-  ['left_out'] = '%s left the instance',
+  ['entered_into'] = '%s entrou na instância',
+  ['left_out'] = '%s saiu da instância',
 }
